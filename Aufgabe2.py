e74a577146cba03582c1954b7b1ef3d9bcecbc59
senda1988 <zidi.senda@gmail.com>
2025-01-01 14:10:49 +0100

    Update Aufgabe2.py

--- Aufgabe2.py
+++ Aufgabe2.py
@@ -1,1 +1,18 @@
 spiegelei = ["Ei", "Salz", "Paprika", "Pfeffer"]
+pasta = ["Pasta", "creme fraiche", "Käse", "Spinat", "salz", "Pfeffer"]
+salat = ["Salat", "Tomaten", "Gurke", "Olivenöl"]
+
+
+def Rezept():
+    rezept = int(
+        input("welches Rezept ausgegeben werden soll 1. Spiegelei 2. Pasta 3. Salat")
+    )
+    if rezept == 1:
+        print(f"Die Zutaten für Spiegelei sind: {spiegelei}")
+    elif rezept == 2:
+        print(f"Die Zutaten für Pasta sind: {pasta}")
+    elif rezept == 3:
+        print(f"Die Zutaten für Salat sind: {salat}")
+
+
+Rezept()
